9d0ec11fd9fa08274a1d0edce034d01d8eb2177a
masacoco <masaonakama0329@gmail.com>
2021-11-25 19:12:31 +0900

    first commit

---
+++ PRACTICESWIFT/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PRACTICESWIFT
+//
+//  Created by Masao Nakama on 11/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+            .foregroundColor(Color.green)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+.previewInterfaceOrientation(.portraitUpsideDown)
+    }
+}
---
+++ PRACTICESWIFT/PRACTICESWIFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PRACTICESWIFTApp.swift
+//  PRACTICESWIFT
+//
+//  Created by Masao Nakama on 11/25/21.
+//
+
+import SwiftUI
+
+@main
+struct PRACTICESWIFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
