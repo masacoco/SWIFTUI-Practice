16e965fd81e9a6d19e6c44beb085e5b1fc6d1957
masacoco <masaonakama0329@gmail.com>
2021-11-25 19:28:26 +0900

    add navigationbarItem explanation

--- PRACTICESWIFT/ContentView.swift
+++ PRACTICESWIFT/ContentView.swift
@@ -9,11 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, SwiftUI!")
-            .foregroundColor(Color.green)
-            .padding()
+        NavigationView{
+            Text("Hello")
+//            右上にアイテムと出す→trailing, 左上→leading
+                .navigationBarItems(trailing:
+                                        Text("アイテム")
+                )
+        }
+//Listとナビゲーションバー
+//        List{
+//            NavigationView{
+//                Text("Hello")
+//                    .navigationBarTitle(Text("タイトル"))
+//            }
+//            Text("りんご")
+//                .foregroundColor(Color.red)
+//                .padding()
+//            Text("レモン")
+//                .foregroundColor(Color.yellow)
+//                .padding()
+//            Text("ブドウ")
+//                .foregroundColor(Color.purple)
+//                .padding()
+//        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
